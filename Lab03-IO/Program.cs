54efefdb3896c29cb91749ab94dffb52a553e884
David Dicken <davidfromseattle@gmail.com>
2021-01-13 15:25:36 -0800

    wrote code for first challenge

--- Lab03-IO/Program.cs
+++ Lab03-IO/Program.cs
@@ -6,7 +6,23 @@ namespace Lab03_IO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ProductOfThree(getNums()));
+        }
+
+        static string getNums()
+        {
+            Console.WriteLine("Please enter three numbers seperated by a space.");
+
+            return Console.ReadLine();
+        }
+
+        static decimal ProductOfThree(String str)
+        {
+            string[] nums = str.Split(" ");
+
+            if (nums.Length < 3) return 0;
+
+            return Convert.ToDecimal(nums[0]) * Convert.ToDecimal(nums[1]) * Convert.ToDecimal(nums[2]);
         }
     }
 }
---
+++ ProductOfThreeTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab03_IO;
+
+namespace ProductOfThreeTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void HappyPath()
+        {
+            Assert.Equal(24, progr)
+        }
+    }
+}
